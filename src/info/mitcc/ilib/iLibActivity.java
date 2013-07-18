e7b5fe5882fae83979c6e64764a89bc8806eb228
mitcc <mitcc@qq.com>
2013-07-18 19:02:12 +0800

    add books detail information

--- src/info/mitcc/ilib/iLibActivity.java
+++ src/info/mitcc/ilib/iLibActivity.java
@@ -1,8 +1,10 @@
 package info.mitcc.ilib;
 
+import info.mitcc.bean.BookBean;
 import info.mitcc.bean.DocNumberBean;
 import info.mitcc.bean.SetNumberBean;
 import info.mitcc.http.HttpClientConnector;
+import info.mitcc.sax.BookInfoXMLParse;
 import info.mitcc.sax.DocNumberXMLParse;
 import info.mitcc.sax.SetNumberXMLParse;
 import android.app.Activity;
@@ -25,7 +27,9 @@ public class iLibActivity extends Activity {
 	ListView listView;
 	ArrayAdapter<String> listAdapter;
 	
-	DocNumberBean docNumberBean;
+//	DocNumberBean docNumberBean;
+	BookBean bookBean;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,16 +52,27 @@ public class iLibActivity extends Activity {
 				String set_number = HttpClientConnector.getStringByUrl(getSetNumberUrl);
 				SetNumberBean setNumberBean = SetNumberXMLParse.parse(set_number);
 				
-				String getDocNumberUrl = "http://10.10.16.94/X?op=present&set_no=" + setNumberBean.getSet_number()
+//				String getDocNumberUrl = "http://10.10.16.94/X?op=present&set_no=" + setNumberBean.getSet_number()
+//						+ "&set_entry=000000001,000000002,03,04,05&format=marc";
+//				String doc_number = HttpClientConnector.getStringByUrl(getDocNumberUrl);
+//				docNumberBean = DocNumberXMLParse.parse(doc_number);
+				
+				String getBooksInfoUrl = "http://10.10.16.94/X?op=present&set_no=" + setNumberBean.getSet_number()
 						+ "&set_entry=000000001,000000002,03,04,05&format=marc";
-				String doc_number = HttpClientConnector.getStringByUrl(getDocNumberUrl);
-				docNumberBean = DocNumberXMLParse.parse(doc_number);
+				String booksInfosXml = HttpClientConnector.getStringByUrl(getBooksInfoUrl);
+				System.out.println("booksInfosXml = " + booksInfosXml);
+				bookBean = BookInfoXMLParse.parse(booksInfosXml);
 				
-				listAdapter.add("set_number : " + setNumberBean.getSet_number());
 				
-				for(int i = 0; i < docNumberBean.getDoc_number().size(); i++) {
-					listAdapter.add("doc_number[" + i + "] : " + docNumberBean.getDoc_number().get(i));
-				}
+				
+//				listAdapter.add("set_number : " + setNumberBean.getSet_number());
+				
+//				for(int i = 0; i < docNumberBean.getDoc_number().size(); i++) {
+//					listAdapter.add("doc_number[" + i + "] : " + docNumberBean.getDoc_number().get(i));
+//				}
+				
+				for(int i = 0; i < bookBean.getDoc_number().size(); i++) 
+					listAdapter.add("bookName[" + i + "] : " + bookBean.getBookName().get(i));
 			}
 		});
         listView.setAdapter(listAdapter);
@@ -67,7 +82,7 @@ public class iLibActivity extends Activity {
 					long id) {
 				Intent intent = new Intent(iLibActivity.this, DetailsActivity.class);
 				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				intent.putExtra("doc_number", docNumberBean.getDoc_number().get(position).toString());
+				intent.putExtra("doc_number", bookBean.getBookName().get(position).toString());
 				startActivity(intent);
 			}
 		});
