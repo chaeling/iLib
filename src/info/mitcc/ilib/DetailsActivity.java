bb6a770c0c6510264246b2bb62b4844317004a9a
mitcc <mitcc@qq.com>
2013-07-18 14:27:09 +0800

    20130718 update

---
+++ src/info/mitcc/ilib/DetailsActivity.java
@@ -0,0 +1,29 @@
+package info.mitcc.ilib;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DetailsActivity extends Activity {
+	ListView listView;
+	ArrayAdapter<String> detailsAdapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.details);
+		
+		listView = (ListView) findViewById(R.id.detailsListView);
+		detailsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		
+		
+		listView.setAdapter(detailsAdapter);
+		
+		Intent intent = getIntent();
+		String text = intent.getStringExtra("doc_number");
+		detailsAdapter.add(text);
+	}
+}
